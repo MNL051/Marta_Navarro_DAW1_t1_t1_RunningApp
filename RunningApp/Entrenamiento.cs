50f4003198a0ce51dd2014efd1571e9ddc588384
MNL051 <navarrolozano@outlook.com>
2025-02-13 11:22:32 +0100

    Update

---
+++ RunningApp/Entrenamiento.cs
@@ -0,0 +1,8 @@
+class Entrenamiento{
+
+    private string? nombreEntrenamiento;
+    private int? duracion;
+
+    public string?  NombreEntrenamiento{get => nombreEntrenamiento; set => value;}
+    public string?  Duracion{get => duracion; set => value;}
+}
--- RunningApp/obj/Debug/net9.0/RunningApp.AssemblyInfo.cs
+++ RunningApp/obj/Debug/net9.0/RunningApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunningApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050439791f1865561b8135a5b4d842d7c5caad9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1adf09dccae55c0b798da43372c1803984964f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunningApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunningApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
